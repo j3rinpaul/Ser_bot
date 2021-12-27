f46727fffa1d540fe0cb58187a0f1a0c880a6e73
Jerin Paul <jerrymic2255@gmail.com>
2021-12-27 08:24:25 +0530

    rev_1

--- bot.py
+++ bot.py
@@ -1,46 +1,58 @@
 from discord import channel, message
-import discord.ext
+from discord.utils import get
 import discord
 import os
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv('Discord_Token')
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+client = commands.Bot(command_prefix='!',help_command=None)
 
+@client.event
+async def on_ready():
+    print(f'Logged on as {client.user}!')
 
-    async def on_message(self,message):
-        if message.author == self.user:
-            return
-        str_name = str(message.author).split("#")
-        str_act = str_name[0]
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    str_name = str(message.author).split("#")
+    str_act = str_name[0]
+    
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hey...\n'+ str_act +"\nHow you Doin..??")
+    if message.content.startswith('!greet'):
+        str_gret = str(message.mentions).split(" ")
+        str_gret = str_gret[2].split("'")
+        str_gret = str_gret[1]
+        await message.channel.send(str_act +" Greeted "+str_gret)
+    if message.content.startswith("💐"):
+        str_gret = str(message.mentions).split(" ")
+        str_gret = str_gret[2].split("'")
+        str_gret = str_gret[1]
+        await message.channel.send(str_act +" Greeted "+str_gret)
+
+@client.event
+async def on_member_join(member):
+    guild = member.guild
+    if guild.system_channel is not None:
+        await guild.system_channel.send(f"Welcome {member.mention} to {guild.name}!") 
         
-        if message.content.startswith('!hello'):
-            await message.channel.send('Hey...\n'+ str_act +"\nHow you Doin..??")
-        if message.content.startswith('!greet'):
-            str_gret = str(message.mentions).split(" ")
-            str_gret = str_gret[2].split("'")
-            str_gret = str_gret[1]
-            await message.channel.send(str_act +" Greeted "+str_gret)
-        if message.content.startswith("💐"):
-            str_gret = str(message.mentions).split(" ")
-            str_gret = str_gret[2].split("'")
-            str_gret = str_gret[1]
-            await message.channel.send(str_act +" Greeted "+str_gret)
 
-
-    async def on_member_join(self, member):
-        guild = member.guild
-        if guild.system_channel is not None:
-            to_send = f"Welcome {member.mention} to {guild.name}!"
-            await guild.system_channel.send(to_send) 
+@client.command()
+async def add_role(ctx,role:discord.Role,user:discord.User):
+    if ctx.author.guild_permissions.administrator:
+        await user.add_role(role)
+        await ctx.send(f'Role Given to {role.mention} to {user.mention}')
+# @client.command()
+# async def remove_role(ctx,role:discord.Role,user:discord.User):
+#     if ctx.author.guild_permissions.administrator:
+#         await user.remove_role(role)
+#         await ctx.send(f'Role {role.mention} removed')
 
 
 
-
-bot = MyClient()
-bot.run(TOKEN)
+client.run(TOKEN)
--- func.py
+++ func.py
@@ -1,2 +1,9 @@
-class joinMsg():
-    
+import discord
+from discord import commands
+from discord.utils import get 
+
+
+
+
+class roleCreation(discord.Client):
+    async def 
