7c315d8c93744a9e7bfa3d3764039c1e839b2fc9
Jerin Paul <jerrymic2255@gmail.com>
2022-01-19 08:56:43 +0530

    rev_1

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import discord
 import random
 from discord.ext import commands
 from discord.ext.commands import Bot
-import msg_content
 from discord.flags import Intents
 import sqlite3
 from dotenv import load_dotenv
@@ -11,6 +10,8 @@ import os
 load_dotenv()
 
 Token = os.getenv('Discord_Token')
+channel_general = 848863747891658786
+
 
 intents = discord.Intents().default()
 intents.members =True
@@ -18,37 +19,37 @@ client=commands.Bot(command_prefix="!",intents=intents)
 
 
 def WelcomeMessage():
-    messages= msg_content.melcow.message()
+    messages= ['Welcome Boss! I hope you are going to have a great time with us. So happy to have you among us',
+                'Welcome! We are honored to receive you like your presence is crucial!',
+                'It is our great pleasure to have you on board! A hearty welcome to you!',
+                'We value our customers more than anything, and your satisfaction is what we aim for! Welcome to you! Thank you for visiting us!']
     return random.choice(messages)
-
 
 @client.event
 async def on_ready():
-    db =sqlite3.connect('data.sqlite')    
-    cursor = db.cursor()
+    dbase =sqlite3.connect('data.sqlite')    
+    cursor = dbase.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS data(
             member_id INT,
             name TEXT
             )
     ''')
-    print("Database = active")
-    print("We have logged in as {0.user}".format(client))
+    print(f"We have logged in as {client.user}")
+    print("Database activated")
 
 
 async def on_member_join(member):
-    guild = client.get_guild(848863747891658786)
-    channel = guild.get_channel(848863747891658786)
-    await channel.send("Welcome to the new world mr {} :waving: ".format(member.mention)) 
+    guild = client.get_guild(channel_general)
+    channel = guild.get_channel(channel_general)
+    await channel.send(f"Welcome to the new world {member.mention} :waving: \n Hope to see you at Frontline") 
     await member.send(str(WelcomeMessage()))
 
-
-
-        
+      
 @client.event
 async def on_raw_reaction_add(reaction):
     channel=client.get_channel(reaction.channel_id)
-    channel2=client.get_channel(848863747891658786)
+    channel2=client.get_channel(channel_general)
     message=await channel.fetch_message(reaction.message_id)
     emoji=reaction.emoji.name
     await channel2.send(f"{reaction.member.name} reacted to {message.author.name}'s message with {emoji}")
@@ -57,17 +58,27 @@ async def on_raw_reaction_add(reaction):
 @client.command()
 async def role(ctx,*args):
     user=ctx.author
-    channel = client.get_channel(848863747891658786)
+    channel = client.get_channel(channel_general)
     role=await ctx.guild.create_role(name=args[0])
-    # print(f'The {role} role was suscessfully created by {user}')
     await user.add_roles(role)
-    await channel.send(f'{role} was successfully created...')
+    await channel.send(f'Role {role} was successfully created...Horray!!')
 
+@client.command()
+async def add_role(ctx,role:discord.Role,user:discord.Member):
+    if ctx.author.guild_permissions.administrator:
+        await user.add_roles(role)
+        await ctx.send(f'Role  {role.mention} Given to {user.mention}')
+
+@client.command()
+async def remove_role(ctx,role:discord.Role,user:discord.Member):
+    if ctx.author.guild_permissions.administrator:
+        await user.remove_roles(role)
+        await ctx.send(f'Role {role.mention} removed from {user.mention}')
 
 @client.command()
 async def register(ctx,user: discord.User):
-    db = sqlite3.connect('data.sqlite')
-    cursor = db.cursor()
+    dbase = sqlite3.connect('data.sqlite')
+    cursor = dbase.cursor()
 
     cursor.execute(f"SELECT name FROM data WHERE member_id ={user.id}")
     result =cursor.fetchone()
@@ -75,34 +86,34 @@ async def register(ctx,user: discord.User):
         sql =(" INSERT INTO data(member_id,name) VALUES(?,?)" )
         val1=(user.id,user.name)
         cursor.execute(sql,val1)
-        await ctx.channel.send(f"The user added to the database \n Username: {user.name}\n UserID: {user}\n Status: Success")
+        await ctx.channel.send(f"User added to the database \n Name: {user.name}\n UserID: {user}\n Status: Success")
 
     else:
-        await ctx.channel.send(f"The user already exist \n Username:{user} \n Name:{user.name}")
+        await ctx.channel.send(f"User already exist \n Name:{user} \n Name:{user.name}")
 
 
 
-    db.commit()
+    dbase.commit()
     cursor.close()
-    db.close()
+    dbase.close()
 
 
 @client.command()
 @commands.has_role("admin") 
 async def names(ctx):
-    db = sqlite3.connect('data.sqlite')
-    cursor = db.cursor()
+    dbase = sqlite3.connect('data.sqlite')
+    cursor = dbase.cursor()
 
 
     cursor.execute(f"SELECT name FROM data")
     result =cursor.fetchall()
-    for i in result:       
-        for j in i:
-            await ctx.channel.send(j)
+    for events in result:       
+        for content in events:
+            await ctx.channel.send(content)
 
 
     cursor.close()
-    db.close()
+    dbase.close()
 
 @client.event
 async def on_comman_error(ctx,error):
